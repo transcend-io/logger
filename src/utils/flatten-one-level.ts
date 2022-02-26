e78163ca5ea75c9b69c572cd85da088ead95ed9c
michaelfarrell76 <mike@transcend.io>
2022-02-26 12:27:18 -0800

    Updates dependencies

--- src/utils/flatten-one-level.ts
+++ src/utils/flatten-one-level.ts
@@ -1,6 +1,9 @@
 const { flat } = Array.prototype;
 
 export const flattenOneLevel = (flat as undefined | typeof flat)
-  ? (list: any[]) => list.flat(1)
-  : (list: any[]) =>
+  ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (list: any[]) => list.flat(1)
+  : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (list: any[]) =>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       list.reduce((acc: any[], val: any[]) => acc.concat(val), []);
